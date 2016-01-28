00417f056d64cf217cd89309ee48300c68a97d1d
Rohan Poddar <rohan.poddar@gmail.com>
2016-01-27 18:59:58 -0600

    Rohan | #0 | Created scripts for installation, cache clearing, and linking the project inside vagrant

---
+++ scripts/vagrant-database.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant-functions.sh
+USER=apache
+
+if [ "$#" == "0" ]; then
+	FOLDER="app"
+else
+	FOLDER="$1"
+fi
+
+run_in_vagrant -c "sudo rm -rf /var/www/cs-core"
+run_in_vagrant -c "sudo ln -s /campusSportswear/cs-core /var/www/cs-core"
+run_in_vagrant -c "cd /var/www/cs-core && sudo php app/console oro:migration:load"
---
+++ scripts/vagrant-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant-functions.sh
+
+USER=apache
+EMAIL=rohan.poddar@gmail.com
+FIRST_NAME=Rohan
+LAST_NAME=Poddar
+PASSWORD=admin123
+ORG_NAME=campusSports
+APPLICATION_URL=https://192.168.33.10
+
+if [ "$#" == "0" ]; then
+	FOLDER="app"
+else
+	FOLDER="$1"
+fi
+
+run_in_vagrant -c "sudo rm -rf /var/www/cs-core"
+run_in_vagrant -c "sudo ln -s /campusSportswear/cs-core /var/www/cs-core"
+run_in_vagrant -c "sudo rm -rf /var/resources/*"
+run_in_vagrant -c "cd /var/www/cs-core && sudo php app/console oro:install --force --drop-database --env prod --timeout=999999999 --user-email=$EMAIL, --user-firstname=$FIRST_NAME, --user-lastname=$LAST_NAME, --user-password=$PASSWORD --organization-name=$ORG_NAME --application-url=$MACHINE_IP --sample-data=no"
+run_in_vagrant -c "cd /var/resources && sudo chown -R apache:apache *"
